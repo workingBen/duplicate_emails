938973da44cf9aaa807e6337b979ec02f7a6b638
Ben Pellow <ben@campuscred.com>
2011-06-22 16:35:03 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DuplicateEmails::Application.config.secret_token = 'f36add49dbd28dfe10cc7d1688aa1f72f9bfc610aa3ea38df73a58de3a72c72032a3e7167d9ed90efc9ff112734769b4b303a98eadf200e509e382452f55b4f0'
